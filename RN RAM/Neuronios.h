6879e40466fb003eb39175351194dced4a304653
carolesj <caroline.jesuino.silva@usp.br>
2017-06-29 15:42:26 -0300

    Comentários adicionados

--- RN RAM/Neuronios.h
+++ RN RAM/Neuronios.h
@@ -14,22 +14,30 @@ typedef struct neuronio {
     int * saida;
 } Neuronio;
 
-
+//Acessa as posições no vetor de saída do neurônio dadas pela onda recebida e seta essas posições como 1
+//Recebe a lista de neurônios e o vetor processado de exemplos para treinamento
 void aprendizado (Neuronio * lista, int * pontos);
 
 
+//Pré processa os dados do arquivo para poder fazer o aprendizado
+//Recebe a lista de neurônios e o nome do arquivo
 void treinamento (Neuronio * lista, const char * arquivo);
 
 
+//Imprime o número de uns setados em cada classe
 void nUns (Neuronio ** n);
 
 
+//Aloca os neurônios
 Neuronio * criaNeuronios ();
 
 
+//Libera os neurônios
 void liberaNeuronios (Neuronio * n);
 
 
+//Passa uma entrada na rede e dá a resposta reconhecida
+//Recebe a lista de neurônios, as entradas processadas e o número de entradas
 int Resultado (Neuronio ** n, int *** entrada, int n_entradas);
 
 #endif
